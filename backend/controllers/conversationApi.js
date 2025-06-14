6a81d54c74e739dd052eca94afc6117b94330597
RokhayaNC <rokhaya0805@gmail.com>
2025-06-14 00:39:24 +0000

    --

--- backend/controllers/conversationApi.js
+++ backend/controllers/conversationApi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Conversation = require('../models/Conversation');
+const Conversation = require('../models/conversation.js');
 const User = require('../models/User'); // pour récupérer pseudo/avatar
 
 // GET /api/conversations/:userId
